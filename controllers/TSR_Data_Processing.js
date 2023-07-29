35a1538a766da7830130a72ef9f8a88252d1a5c4
akshdesai1 <desaiaksh82@gmail.com>
2023-07-29 15:10:19 +0530

    Modify Css and Add Profile Icon and Logout Functionallity

--- controllers/TSR_Data_Processing.js
+++ controllers/TSR_Data_Processing.js
@@ -14,17 +14,17 @@ const Login = async (req, res) => {
 
         if (req.method == "POST") {
             const { number, password } = req.body;
-            console.log('req.body', req.body);
             const user = await User.findOne({ mobile_no: number });
             // console.log('user', user);
             if (!user) {
-                return res.json({
-                    error: "This Mobile-No is not exits. please try with another one. ",
+                return res.render('login', {
+                    error: true,
+                    msg: "This Mobile-No is not exits. please try with another one. ",
                 });
             }
             const compare = bcrypt.compareSync(password, user.password);
             if (!compare) {
-                return res.json({ error: "Password is not match." });
+                return res.render('login', { error: true, msg: "Password is not match." });
             }
 
             req.session.name = user.name;
@@ -42,7 +42,7 @@ const Login = async (req, res) => {
             return res.redirect("/profile");
         }
         else {
-            return res.render("login");
+            return res.render("login", { error: false });
         }
 
     } catch (error) {
@@ -57,23 +57,23 @@ const Signup = async (req, res) => {
 
             const a = await User.findOne({ mobile_no: mobile_no });
             if (a) {
-                return res.json({
-                    error:
-                        "request with this Mobile No already exists. please try with another one.",
+                return res.render('signup', {
+                    error: true,
+                    msg: "request with this Mobile No already exists. please try with another one."
                 });
             }
 
             if (adhar_number.length != 16) {
-                return res.json({
-                    error:
-                        "Length Of Aadhar Number Must be 16",
+                return res.render('signup', {
+                    error: true,
+                    msg: "Length Of Aadhar Number Must be 16",
                 });
             }
 
             if (mobile_no.length != 10) {
-                return res.json({
-                    error:
-                        "Length Of Mobile Number Must be 10",
+                return res.render('signup', {
+                    error: true,
+                    msg: "Length Of Mobile Number Must be 10",
                 });
             }
 
@@ -91,7 +91,7 @@ const Signup = async (req, res) => {
             return res.redirect("/profile");
         }
         else {
-            return res.render("signup");
+            return res.render("signup", { error: false });
         }
 
     } catch (error) {
@@ -104,11 +104,12 @@ const Profile = async (req, res) => {
         if (req.session.name) {
             if (req.method == "POST") {
                 const user = await User.findOne({ mobile_no: req.session.mobile_no });
-                const result = await User.findByIdAndUpdate(user._id, req.body);
-                return res.redirect("/profile");
+                const result = await User.findByIdAndUpdate(user._id, { $set: req.body });
+                const result1 = await User.findByIdAndUpdate(user._id, { $set: req.body });
+                return res.render("profile", { update: true, msg: "Updated Profile Successfully.", user: result1, reload: true });
             }
             const user = await User.findOne({ "mobile_no": req.session.mobile_no });
-            return res.render("profile", { user });
+            return res.render("profile", { update: false, user });
         }
         else {
             return res.redirect("/login");
@@ -126,13 +127,14 @@ const UploadDocuments = async (req, res) => {
                 const result = await User.findByIdAndUpdate(user._id, req.body);
                 // return res.redirect("/Upload-Documents");
 
-                const data = await User.findByIdAndUpdate(user._id, { payment_screenshot: `http://127.0.0.1:8080/document/${req.files[0].filename}`, tsr_report: `http://127.0.0.1:8080/document/${req.files[1].filename}` });
+                const data = await User.findByIdAndUpdate(user._id, { payment_screenshot: `https://aadhaareodreport.azurewebsites.net/document/${req.files[0].filename}`, tsr_report: `https://aadhaareodreport.azurewebsites.net/document/${req.files[1].filename}` });
 
                 // return res.json({ "Success": true, 'path0': `http://127.0.0.1:8080/document/${req.files[0].filename}`, 'path1': `http://127.0.0.1:8080/document/${req.files[1].filename}` })
-                return res.redirect('/Upload-Documents');
+                return res.render('uploadDocuments', { success: true, msg: "Documents Uploaded Successfully.", user });
             }
-            // const user = await User.findOne({ "mobile_no": req.session.mobile_no });
-            return res.render("uploadDocuments.ejs");
+            const user = await User.findOne({ "mobile_no": req.session.mobile_no });
+            // console.log('user', req.session.mobile_no);
+            return res.render("uploadDocuments.ejs", { user, success: false });
         }
         else {
             return res.redirect("/login");
